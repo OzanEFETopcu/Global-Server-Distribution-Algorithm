33f5af05c38a81c4fc7d275204fdccb3b3730e10
OzanEFETopcu <ozanefe.topcu@gmail.com>
2024-10-29 12:23:49 +0200

    updating

--- serverMeasurements/measurementServerSide.py
+++ serverMeasurements/measurementServerSide.py
@@ -1,6 +1,6 @@
 # Set the environment variables this way:
 #   export FLASK_APP={app_name}.py
-# Run the python app with "flask run"
+# Run the python app with "python -m flask --app .\measurementServerSide.py run --port 8000"
 
 from flask import Flask
 app = Flask(__name__)
@@ -10,4 +10,4 @@ def get_status():
     return {"success": "Success"}, 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)

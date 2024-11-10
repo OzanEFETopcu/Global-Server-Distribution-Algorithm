bf1cfc5c74fedf4b580d3930fa4c7c1bb569254a
OzanEFETopcu <ozanefe.topcu@gmail.com>
2024-11-10 05:05:28 +0200

    Finished the server and process architecture only scaling left

---
+++ optimizationAlgo/data/mqttTestApp/subscribe/appConst.h
@@ -0,0 +1,82 @@
+// constants.h
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <map>
+#include <string>
+#include <cmath> // For std::ceil
+
+// Namespace to encapsulate the constants and functions, avoiding polluting the global namespace.
+namespace Constants {
+
+// Average cloud boot duration in seconds
+inline constexpr double averageCloudBootDuration = 11.381333;
+
+// Memory and processor coefficients
+inline constexpr double memoryCoefficient = 2.5; // GB
+inline constexpr double logicalProcessorCoefficient = 1.4; // vCPU or Logical Processors
+inline constexpr double memoryConstant = 3; // GB
+inline constexpr double logicalProcessorConstant = 2.2; // vCPU or Logical Processors
+inline constexpr double averageApplicationExecutionDuration = 60; // seconds. 200 minutes in real life
+
+// Function declarations
+inline int vCPUReqCalculator(int totalProcessNum) {
+    double estimateRequirement = logicalProcessorCoefficient * (totalProcessNum - 1) + logicalProcessorConstant;
+    return std::ceil(estimateRequirement);
+}
+
+inline int memoryReqCalculator(int totalProcessNum) {
+    double estimateRequirement = memoryCoefficient * (totalProcessNum - 1) + memoryConstant;
+    return std::ceil(estimateRequirement);
+}
+
+// Average regional service pricing in USD
+inline const std::map<std::string, float> oregonServerPricing = {
+    {"c4", 0.1430},
+    {"c8", 0.2859},
+    {"c16", 0.5719},
+    {"c32", 1.1437},
+    {"c52", 1.8655},
+    {"c88", 3.1869},
+};
+
+inline const std::map<std::string, float> londonServerPricing = {
+    {"c4", 0.1774},
+    {"c8", 0.3547},
+    {"c16", 0.7095},
+    {"c32", 1.4190},
+    {"c52", 2.3217},
+    {"c88", 3.9969},
+};
+
+inline const std::map<std::string, float> singaporeServerPricing = {
+    {"c4", 0.1706},
+    {"c8", 0.3412},
+    {"c16", 0.6824},
+    {"c32", 1.3648},
+    {"c52", 2.2316},
+    {"c88", 3.8360},
+};
+
+// Average inter/intra-regional service latency in milliseconds
+inline const std::map<std::string, float> oregonToDest = {
+    {"oregon", 20},
+    {"singapore", 380},
+    {"london", 260},
+};
+
+inline const std::map<std::string, float> singaporeToDest = {
+    {"oregon", 370},
+    {"singapore", 1},
+    {"london", 400},
+};
+
+inline const std::map<std::string, float> londonToDest = {
+    {"oregon", 320},
+    {"singapore", 320},
+    {"london", 1},
+};
+
+} // namespace Constants
+
+#endif // CONSTANTS_H
--- optimizationAlgo/data/mqttTestApp/subscribe/mainReceiveCenter.cpp
+++ optimizationAlgo/data/mqttTestApp/subscribe/mainReceiveCenter.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 int main()
 {
-    RegionalAlgo regionOregon("Oregon");
-    RegionalAlgo regionLondon("London");
-    RegionalAlgo regionSingapore("Singapore");
-
     // Create a vector to store the algorithms for scaling
-    vector<RegionalAlgo> regions= {regionOregon, regionLondon, regionSingapore};
+    vector<unique_ptr<RegionalAlgo>> regions;
     // Create a vector to store the threads
     vector<thread> threads;
+
+    regions.push_back(make_unique<RegionalAlgo>("Oregon"));
+    regions.push_back(make_unique<RegionalAlgo>("London"));
+    regions.push_back(make_unique<RegionalAlgo>("Singapore"));
 
 
     // Launch a thread for each region
     /*for (auto& region : regions) {
         threads.emplace_back([&region]() { region.messageReceiver(); });  // Pass the region name to each thread
     }*/
-    regions[0].messageReceiver();
+    regions[0]->messageReceiver();
 
     // Wait for all threads to finish
-    for (auto& t : threads) {
+    /*for (auto& t : threads) {
         t.join();
-    }
+    }*/
 
     return 0;
 
